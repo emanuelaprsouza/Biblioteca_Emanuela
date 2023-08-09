629b48be21a5539b922fad31c3b0069f22235b47
CJ3016501 <CJ3016501@LAB01-PC07>
2023-08-09 12:51:13 -0300

    Adicionar arquivos de projeto.

---
+++ Emanuela3B/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Emanuela3B
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btncow = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txbNome = new System.Windows.Forms.TextBox();
+            this.TxbPront = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btncow
+            // 
+            this.btncow.BackColor = System.Drawing.Color.Red;
+            this.btncow.Font = new System.Drawing.Font("Dutch801 XBd BT", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncow.ForeColor = System.Drawing.Color.Black;
+            this.btncow.Location = new System.Drawing.Point(343, 55);
+            this.btncow.Name = "btncow";
+            this.btncow.Size = new System.Drawing.Size(256, 154);
+            this.btncow.TabIndex = 0;
+            this.btncow.Text = "Aperta!";
+            this.btncow.UseVisualStyleBackColor = false;
+            this.btncow.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = " NOME";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txbNome
+            // 
+            this.txbNome.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbNome.Location = new System.Drawing.Point(70, 69);
+            this.txbNome.Name = "txbNome";
+            this.txbNome.Size = new System.Drawing.Size(163, 33);
+            this.txbNome.TabIndex = 2;
+            this.txbNome.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // TxbPront
+            // 
+            this.TxbPront.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxbPront.Location = new System.Drawing.Point(70, 180);
+            this.TxbPront.Name = "TxbPront";
+            this.TxbPront.Size = new System.Drawing.Size(163, 29);
+            this.TxbPront.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(67, 148);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "PRONTUARIO";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(694, 262);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxbPront);
+            this.Controls.Add(this.txbNome);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btncow);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btncow;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txbNome;
+        private System.Windows.Forms.TextBox TxbPront;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Emanuela3B/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Emanuela3B
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String nome = txbNome.Text;
+            String enrollment = TxbPront.Text;
+
+            String message = "Nome: " + nome +
+                            "\nMatrícula: " + enrollment;
+
+            MessageBox.Show(
+                message,
+                "ATENÇÃO",
+                MessageBoxButtons.OKCancel,
+                MessageBoxIcon.Information           
+                );
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
